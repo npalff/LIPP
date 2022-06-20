9049c01e7c996dfdbe466323f7df22c4129a2725
Nicolau Pereira Alff <npalff@inf.ufrgs.br>
2022-06-20 15:32:08 +0200

    benchmark code creation

---
+++ src/benchmark/benchmark.cpp
@@ -0,0 +1,12 @@
+#include <lipp.h>
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+
+// Modify these if running your own workload
+//#define KEY_TYPE double
+//#define PAYLOAD_TYPE double
+
+#define KEY_TYPE int64_t
+#define PAYLOAD_TYPE int64_t
